584226343dd83ae215b0d786fc60399358038c61
Florian Morath <morathflorian@gmail.com>
2017-12-03 22:24:10 +0100

    restore commit "normalised quantification error computation"

--- task3/example.py
+++ task3/example.py
@@ -25,20 +25,17 @@ def mapper(key, value):
     images = value
 
     # do D^2-sampling -> results in a bicriteria approximation
-    n = randint(N) # random index between 0 and N (u.a.r)
-    b = images[n] # sample the first point (row vector)
-    dist = np.empty((0, N)) # contains the distances of each sampled point to every point in images
+    dist = np.empty((0, N)) # 2D array containing the distances of each sampled point to every point in images
     # first row of distances contains the distances of each point in images to the first sampled point
     # second row contains distances of each point to the second sampled point and so on
     b = images[randint(N)] # sample the first point u.a.r (is a row vector)
     for i in range(1, k):
         # for each data point (row in the images matrix), subtract point b and calculate the squared norm of the distance
-        # add distance of every point in images, as a new row to the dist matrix
-        norms = norm(images - b, axis=1)**2
-        dist = np.vstack((dist, norms))
-        current_min_dist = dist.min(axis=0) # holds for every point of the data set the current min distance to the sampled points (centers)
-        probabilities = current_min_dist/current_min_dist.sum()
-        b = images[choice(N, p=probabilities)] # sample a new point with probability proportional to the minimum distance to the already sampled points
+        # add distance of every point in images to the new sampled point b
+        dist = np.vstack((dist, norm(images - b, axis=1)**2))
+        current_min_dist = dist.min(axis=0) # compute for every point of the data set the current minimum distance to the samled points (centers)
+        p = current_min_dist/current_min_dist.sum()
+        b = images[choice(N, p=p)] # sample a new center with probability proportional to the minimum distance to the already sampled points
 
     # use importance sampling to construct a coreset
     Bi_indexes = dist.argmin(axis=0) # array containing the index of the closest sampled center for each point in images
@@ -50,45 +47,38 @@ def mapper(key, value):
     c_phi = current_min_dist.sum()/N
     sum_Bi_min_dist = [sum([current_min_dist[x] for x in Bi[i]]) for i in range(k)]
     Bi_part = [2.0*alpha*sum_Bi_min_dist[i]/(len(Bi[i])*c_phi) + 4.0*N/len(Bi[i]) if Bi[i] else 0 for i in range(k)] # second summand of sampling probability
-    sensitivity = np.array([alpha*current_min_dist[x]/c_phi + Bi_part[Bi_indexes[x]] for x in range(N)])
-    probabilities = sensitivity/sensitivity.sum() # compute sampling probability for every point in images
-    weights = 1.0/(m*probabilities) # compute weight for every point in images
-    coreset = [(images[s], weights[s]) for s in choice(N, size=m, p=probabilities)] # sample m points using the calculated probabilities and construct the coreset
+    s = np.array([alpha*current_min_dist[x]/c_phi + Bi_part[Bi_indexes[x]] for x in range(N)])
+    p = s/s.sum() # compute sampling probability for every point in images
+    w = 1.0/(m*p) # compute weight for every point in images
+    coreset = [(images[s], w[s]) for s in choice(N, size=m, p=p)] # sample m points using the calculated probabilities and construct the coreset
 
     print len(coreset)
     yield 0, coreset
 
 
 def reducer(key, values):
-    # key: key from mapper used to aggregate (= 0 for all mappers)
-    # values: list of all value for that key (list of (image, weight) pairs)
+    # key: key from mapper used to aggregate
+    # values: list of all value for that key
     coreset = values    # union of all coresets received from reducers
 
-    # k-means algorithm performed on coreset
-    # each restart has a different initial guess of the k centers (chosen u.a.r)
-    # centers.shape = (restarts, k, D)
+    # Perform k-means with multiple random restarts
+    # Initialize the centers for each restart by sampling from the coreset with uniform probability
     centers = np.array([[coreset[i][0] for i in choice(len(coreset), size=k)] for r in range(restarts)])
 
     for e in range(epochs):
-        # sum of the points closest to each center for each restart
-        point_sum = [[0 for i in range(k)] for r in range(restarts)]
-        # number of points closest to each center for each restart
-        point_count = [[0 for i in range(k)] for r in range(restarts)]
+        point_sum = [[0 for i in range(k)] for r in range(restarts)] # sum of the points closest to each center for each restart
+        point_count = [[0 for i in range(k)] for r in range(restarts)] # number of points closest to each center for each restart
         for x,w in coreset:
-            # find the closest center to the point x for each restart
-            closests_centers = norm(centers - x, axis=2).argmin(axis=1)
+            c = norm(centers - x, axis=2).argmin(axis=1) # find the closest center to the point x for each restart
             for r in range(restarts):
-                # update the sum
-                point_sum[r][closests_centers[r]] += w*x
-                # update the count
-                point_count[r][closests_centers[r]] += w
-        # update the centers
-        centers = np.array([[point_sum[r][i]/point_count[r][i] if point_count[r][i] != 0 else centers[r][i] for i in range(k)] for r in range(restarts)])
+                point_sum[r][c[r]] += w*x # update the sum
+                point_count[r][c[r]] += w # update the count
+        centers = np.array([[point_sum[r][i]/point_count[r][i] if point_count[r][i] != 0 else centers[r][i] for i in range(k)] for r in range(restarts)]) # update the centers
 
-    # Compute the normalized quantization error on the coreset for a specific set of centers
     X = [v[0]for v in coreset]
     W = [v[1]for v in coreset]
 
+    # Compute the normalized quantization error on the coreset for a centers configuration
     def NQE(centers):
         score = 0
         for i in range(len(X)):
@@ -98,10 +88,10 @@ def reducer(key, values):
             score += distance * W[i]
         return score/(N*10)
 
-    # Choose the centers which result in the best NQE
-    score_candidates = [NQE(centers[r]) for r in range(restarts)]
-    print "Scores candidates:", score_candidates
-    print "Best score:", min(score_candidates)
-    i_min = np.array(score_candidates).argmin()
+    # Select the result of the repetition with the smallest error
+    scores = [NQE(centers[r]) for r in range(restarts)]
+    print "Restart scores:", scores
+    print "Best score:", min(scores)
+    min_index = np.array(scores).argmin()
 
-    yield centers[i_min]
+    yield centers[min_index]
